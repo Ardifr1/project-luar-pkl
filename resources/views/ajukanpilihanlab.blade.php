4b585dc264c4a15ad2d2d9fcd8653abb09f0049c
AbhiRizkyDika <dikaasbaburizki@gmail.com>
2026-08-18 10:44:07 +0700

    adla

---
+++ resources/views/ajukanpilihanlab.blade.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajukan pilihan lab</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <style>
+body{
+            background:#222;
+        }
+
+        .phone{
+            max-width:412px;
+            min-height:917px;
+            margin:20px auto;
+            background:#fff;
+        }
+
+        .header{
+            background:#102C6B;
+            height:100px;
+            justify-content:space-between;
+            display:flex;
+        }
+
+
+        .menu-card1 {
+  width: 400px;      
+  height: 40px;
+  background-color: #d9d9d9;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding:  20px;
+  border: 1px solid #aaa;
+  border-radius: 6px;
+}
+
+.breadcrumb a {
+  color: #007bff;
+  text-decoration: none;
+  margin-right: 5px;
+}
+
+ /* =========================
+           DROPDOWN HAMBURGER
+           ========================= */
+
+        .hamburger-container{
+            position:relative;
+        }
+
+        .hamburger-dropdown{
+            display:none;
+
+            position:absolute;
+
+            top:70px;
+            right:10px;
+
+            width:180px;
+
+            background:#D9D9D9;
+
+            border-radius:10px;
+
+            overflow:hidden;
+
+            z-index:1000;
+        }
+
+        .hamburger-dropdown.show{
+            display:block;
+        }
+
+        .hamburger-dropdown a{
+            display:block;
+
+            padding:15px;
+
+            color:#222;
+
+            text-decoration:none;
+
+            text-align:center;
+
+            font-size:14px;
+        }
+
+        .hamburger-dropdown a:hover{
+            background:#c5c5c5;
+        }
+
+        .hamburger-dropdown form{
+            margin:0;
+        }
+
+        .hamburger-dropdown button{
+            width:100%;
+
+            padding:15px;
+
+            border:none;
+
+            background:#D9D9D9;
+
+            color:#222;
+
+            font-size:14px;
+
+            cursor:pointer;
+        }
+
+        .hamburger-dropdown button:hover{
+            background:#c5c5c5;
+        }
+
+</style>
+
+</head>
+<body>
+<div class="phone">
+
+    <div class="header">
+
+        <img
+            src="{{asset ('gambar/download.png') }}"
+            alt="logo"
+            class="logo"
+            style="width:100px; height:100px; margin-right:10px; border-radius:20%"
+        >
+
+        <!-- HAMBURGER -->
+        <div class="hamburger-container">
+
+            <button
+                class="btn text-white"
+                id="hamburgerBtn"
+                type="button"
+            >
+                <i class="bi bi-list fs-1"></i>
+            </button>
+
+
+            <!-- DROPDOWN -->
+            <div
+                class="hamburger-dropdown"
+                id="hamburgerDropdown"
+            >
+
+                <!-- PROFIL -->
+                <a href="{{ route('profil.guru') }}">
+                    Profil
+                </a>
+
+
+                <!-- UBAH PASSWORD -->
+                <a href="{{ route('ubah.password') }}">
+                    Ubah Password
+                </a>
+
+
+                <!-- LOG OUT -->
+                <form
+                    action="{{ route('logout') }}"
+                    method="POST"
+                >
+                    @csrf
+
+                    <button type="submit">
+                        Log out
+                    </button>
+
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
+
+ <div class="menu-card1">
+  <nav class="breadcrumb">
+    <a href="#">Home</a> >
+    <a href="#">profil</a>
+  </nav>
+
+ </div>
+
+
+<!-- =========================
+     JAVASCRIPT HAMBURGER
+     ========================= -->
+
+<script>
+
+    const hamburgerBtn = document.getElementById('hamburgerBtn');
+
+    const hamburgerDropdown = document.getElementById('hamburgerDropdown');
+
+
+    hamburgerBtn.addEventListener('click', function () {
+
+        hamburgerDropdown.classList.toggle('show');
+
+    });
+
+
+</script>
+
+</body>
+</html>
