e60043872e9ece3c06c6ff61a813b9ce33ddec95
AbhiRizkyDika <dikaasbaburizki@gmail.com>
2026-08-28 15:34:45 +0700

    progres

--- app/Http/Controllers/Controllerubahpassword.php
+++ app/Http/Controllers/Controllerubahpassword.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
-class ControllerPassword extends Controller
+class Controllerpassword extends Controller
 {
     // =========================
     // HALAMAN UBAH PASSWORD
--- resources/views/ajukanpilihanlab.blade.php
+++ resources/views/ajukanpilihanlab.blade.php
@@ -51,15 +51,16 @@
         ========================= */
 
         .menu-card1 {
-            width: 400px;
-            height: 40px;
-            background-color: #d9d9d9;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 20px;
-            border: 1px solid #aaa;
-            border-radius: 6px;
+         width: auto;      
+  height: 40px;
+  margin: 10px auto;
+  background-color: #d9d9d9;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding:  20px;
+  border: 1px solid #aaa;
+  border-radius: 6px;
         }
 
         .breadcrumb {
